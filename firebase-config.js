2763efbda75d67172baa179e108d7238ff318e2d
abdeen ali <city89229@gmail.com>
2026-09-16 01:14:08 +0300

    Finalize correct firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBa6gx-YLPNPHSl00yk_u7kNboCJ6EPdjI",
-    authDomain: "zawya-shams-buffet-1ff53.firebaseapp.com",
-    projectId: "zawya-shams-buffet-1ff53",
-    storageBucket: "zawya-shams-buffet-1ff53.firebasestorage.app",
-    messagingSenderId: "687206309313",
-    appId: "1:687206309313:web:b3081b0dcaa4428d7b6340"
+  apiKey: "AIzaSyBa6gx-YLPNPHS1Q0yk_u7kNbcCJ6EPDjI",
+  authDomain: "zawya-shams-buffet-1ff53.firebaseapp.com",
+  projectId: "zawya-shams-buffet-1ff53",
+  storageBucket: "zawya-shams-buffet-1ff53.firebasestorage.app",
+  messagingSenderId: "687206309313",
+  appId: "1:687206309313:web:b3081b0dcaa4428d7b6340"
 };
 
 const app = initializeApp(firebaseConfig);
