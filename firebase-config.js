1e350d62ee3bebe8dcca9d6dcf57c41955cfbb09
abdeen ali <city89229@gmail.com>
2026-09-16 16:04:43 +0300

    Update and finalize all project files with Firebase and offline redirection

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBa6gx-YLPNPHSlOOyk_u7kNboCJ6EPDjI",
   authDomain: "zawya-shams-buffet-1ff53.firebaseapp.com",
@@ -5,18 +8,6 @@ const firebaseConfig = {
   storageBucket: "zawya-shams-buffet-1ff53.firebasestorage.app",
   messagingSenderId: "687206309313",
   appId: "1:687206309313:web:b3081b0dcaa4428d7b6340"
-};
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
