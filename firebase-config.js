5ab64fa92c764561e03abd41b3b375706cbf19ef
abdeen ali <city89229@gmail.com>
2026-09-16 15:29:15 +0300

    Update Firebase config and redirect offline orders to assistant manager Mazhar Ali

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBa6gx-YLPNPHSlOOyk_u7kNboCJ6EPDjI",
+  apiKey: "AIzaSyBa6gx-YLPNPHSlOOyk_u7kNboCJ6EPdjI",
   authDomain: "zawya-shams-buffet-1ff53.firebaseapp.com",
   projectId: "zawya-shams-buffet-1ff53",
   storageBucket: "zawya-shams-buffet-1ff53.firebasestorage.app",
@@ -9,15 +9,6 @@ const firebaseConfig = {
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
